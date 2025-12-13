1a307eaf30df39f77c7f6667b94da92d0e69856d
Ramesh prabhakar <rameshprabhakar014@gmail.com>
2025-12-13 13:48:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Landing from "./Landing/Landing";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
@@ -11,7 +11,7 @@ import Logout from "./Logout/Logout";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
@@ -21,7 +21,7 @@ function App() {
         <Route path="/logout" element={<Logout />} />
  
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
